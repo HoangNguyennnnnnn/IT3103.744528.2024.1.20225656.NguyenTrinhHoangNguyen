9f2e76fc24ce7b551b417a494ebb0eafadf4e97b
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2024-11-26 13:12:38 +0700

    apply-release-flow

--- Lab03/OtherProjects/src/bai2.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-//Nguyen Trinh Hoang Nguyen 20225656
-public class bai2 {
-	public static void main(String args[]) {
-		System.out.println("Nguyen Trinh Hoang Nguyen 20225656");
-		JOptionPane.showMessageDialog(null,"Hello world!How are you?");
-		System.exit(0);
-	}
-}
