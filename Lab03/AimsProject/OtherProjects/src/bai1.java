d2edc686a22df652deacc2fd4ba96514ecdee91d
Hoàng Nguyên <153731374+HoangNguyennnnnnn@users.noreply.github.com>
2024-11-26 11:51:15 +0700

    Create OtherProjects

---
+++ Lab03/AimsProject/OtherProjects/src/bai1.java
@@ -0,0 +1,8 @@
+//Nguyen Trinh Hoang Nguyen 20225656
+public class bai1 {
+	public static void main(String args[]) {
+		System.out.println("Nguyen Trinh Hoang Nguyen 20225656");
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+	} 
+}
