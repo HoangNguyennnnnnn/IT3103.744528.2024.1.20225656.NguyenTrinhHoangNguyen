9b526861b98297c0f58febd0a77d9a94137a0f41
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2024-11-26 18:58:44 +0700

    op

--- OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/dsai/garbage/GarbageCreator.java
@@ -1,23 +1,20 @@
 package hust.soict.dsai.garbage;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class GarbageCreator {
     public static void main(String[] args) throws IOException {
-        String filename = "C:\\Users\\admin\\Desktop\\HUST\\20241\\OOP_TH\\OtherProjects\\src\\hust\\soict\\dsai\\garbage\\test.txt";
-        byte[] inputBytes = { 0 };
+        String filename = "C:\\Users\\Admin\\Documents\\GitHub\\IT3103.744528.2024.1.20225666.TranPhucSon\\OtherProjects\\hust\\soict\\dsai\\garbage\\src\\sample.txt";
+        byte[] inputBytes = {0};
         long startTime, endTime;
 
         inputBytes = Files.readAllBytes(Paths.get(filename));
         startTime = System.currentTimeMillis();
-        StringBuilder outputStringBuilder = new StringBuilder();
-        for(byte b : inputBytes) {
-            outputStringBuilder.append((char)b);
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char) b;
         }
         endTime = System.currentTimeMillis();
         System.out.println(endTime - startTime);
-
     }
 }
--- OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
+++ OtherProjects/src/hust/soict/dsai/garbage/NoGarbage.java
@@ -1,9 +1,10 @@
+package hust.soict.dsai.garbage;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class NoGarbage {
     public static void main(String[] args) throws IOException {
-        String filename = "C:\\Users\\Admin\\Documents\\GitHub\\IT3103.744528.2024.1.20225666.TranPhucSon\\OtherProjects\\hust\\soict\\dsai\\garbage\\src\\sample.txt";
+        String filename = "C:\\Users\\admin\\Documents\\GitHub\\IT3103.744528.2024.1.20225656.NguyenTrinhHoangNguyen\\OtherProjects\\src\\hust\\soict\\dsai\\garbage\\test.txt";
         byte[] inputBytes = {0};
         long startTime, endTime;
 
