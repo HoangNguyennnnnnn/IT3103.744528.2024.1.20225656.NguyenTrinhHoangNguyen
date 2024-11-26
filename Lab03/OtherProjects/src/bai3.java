337baf283c3fd67414faa06bc566d23914537c3e
Hoàng Nguyên <153731374+HoangNguyennnnnnn@users.noreply.github.com>
2024-11-26 12:24:14 +0700

    Add files via upload

---
+++ Lab03/OtherProjects/src/bai3.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+//Nguyen Trinh Hoang Nguyen 20225656
+public class bai3 {
+	public static void main(String args[]) {
+		System.out.println("Nguyen Trinh Hoang Nguyen 20225656");
+		String result;
+		result = JOptionPane.showInputDialog("Please enter your name:");
+		JOptionPane.showMessageDialog(null,"Hi " + result +" !");
+		System.exit(0);
+	}
+}
