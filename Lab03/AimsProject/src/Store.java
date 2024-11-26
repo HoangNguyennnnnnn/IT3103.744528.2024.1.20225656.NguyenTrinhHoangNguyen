29a554612e02018d800728a9c5323bef41139ad3
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2024-11-26 17:44:33 +0700

    store

---
+++ Lab03/AimsProject/src/Store.java
@@ -0,0 +1,40 @@
+public class Store {
+    //Attribute
+    DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[1000000];
+    int numberOfItemsInStore = 0;
+    //Add DVD to Store
+    public void addDVD(DigitalVideoDisc dvd){
+        if (numberOfItemsInStore < itemsInStore.length){
+            itemsInStore[numberOfItemsInStore] = dvd;
+            numberOfItemsInStore++;
+            System.out.println("Added DVD: " + dvd.getTitle());
+        }
+        else {
+            System.out.println("Store is full");
+        }
+    }
+    //Remove DVD in Store
+    public void removeDVD(DigitalVideoDisc dvd){
+        boolean removed = false;
+        for (int i = 0; i < numberOfItemsInStore; i++){
+            if (itemsInStore[i].equals(dvd)){
+                removed = true;
+                for(int j = i; j < numberOfItemsInStore-1; j++){
+                    itemsInStore[j] = itemsInStore[j+1];
+                }
+                numberOfItemsInStore--;
+                System.out.println("Removed DVD: " + dvd.getTitle());
+                break;
+            }
+        }
+        if (!removed){
+            System.out.println("DVD not found!");
+        }
+    }
+    //Print líst DVD in Store
+    public void printListDVD(){
+        for (int i = 0; i < numberOfItemsInStore; i++){
+            System.out.println("DVD: " + itemsInStore[i].toString());
+        }
+    }
+}
