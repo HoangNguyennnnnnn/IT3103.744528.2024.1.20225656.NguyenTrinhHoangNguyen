54e35b380e0616f3ae9b2d1279ff2d38ec5652b7
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2024-11-26 11:22:13 +0700

    Classifier Member and Instance Member

--- Lab03/AimsProject/src/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/DigitalVideoDisc.java
@@ -1,25 +1,29 @@
 public class DigitalVideoDisc {
     // Attribute
+    private static int nbDigitalVideoDiscs = 0;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+    private int id;
     // Constructor
     public DigitalVideoDisc(String title) {
         this.title = title;
-
+        this.id = nbDigitalVideoDiscs++;
     }
     public DigitalVideoDisc(String title, String category, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
+        this.id = nbDigitalVideoDiscs++;
     }
     public DigitalVideoDisc(String title, String category, String director, float cost) {
         this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
+        this.id = nbDigitalVideoDiscs++;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -27,8 +31,9 @@ public class DigitalVideoDisc {
         this.director = director;
         this.length = length;
         this.cost = cost;
+        this.id = nbDigitalVideoDiscs++;
     }
-        // Getter
+    // Getter
     public String getTitle() {
         return title;
     }
@@ -43,5 +48,23 @@ public class DigitalVideoDisc {
     }
     public float getCost() {
         return cost;
+    }
+    public int getId() {
+        return id;
+    }
+
+    //Setter
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //Method
+    public String toString()
+    {
+        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost +" $ ";
+    }
+    public boolean isMatch(String title)
+    {
+        return this.title.equals(title);
     }
 }
