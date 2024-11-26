0e67541ea9c446386106d77f1a9468a8c578cf86
Hoàng Nguyên <nthoangnguyen1308@gmail.com>
2024-11-26 18:01:16 +0700

    create op

--- Lab01/bai1.java
+++
@@ -1,9 +0,0 @@
-package week1;
-//Nguyen Trinh Hoang Nguyen 20225656
-public class bai1 {
-	public static void main(String args[]) {
-		System.out.println("Nguyen Trinh Hoang Nguyen 20225656");
-		System.out.println("Xin chao \n cac ban!");
-		System.out.println("Hello \t world!");
-	} 
-}
